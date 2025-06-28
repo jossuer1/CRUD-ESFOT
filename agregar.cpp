418a75f6e19ef9f85517f56976e316d5c6cc5bf1
jossuer1 <josue.patino@epn.edu.ec>
2025-06-27 23:08:54 -0500

    nota : parametro para el modelo de carros

--- agregar.cpp
+++ agregar.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <clocale>
+using namespace std;
+
+//CREACION DE CRUD AUTOS
+void agregarAutos(){
+    int id;
+    string marca;
+    string modelo;
+    int año;
+    double precio; 
+}
 
