6a67db5fd2387553fbfc08a50609995a65408ab0
jossuer1 <josue.patino@epn.edu.ec>
2025-06-29 15:43:40 -0500

    correcion de struc y se incluye una funcion

--- agregar.cpp
+++ agregar.cpp
@@ -5,11 +5,28 @@
 using namespace std;
 
 //CREACION DE CRUD AUTOS
-void agregarAutos(){
+
+struct vehiculos{
     int id;
     string marca;
     string modelo;
     int año;
     double precio; 
+};
+vector<vehiculos>listaVehiculos;
+
+vector<vehiculos> leer(){
+	return listaVehiculos;
+};
+
+void agregar(const auto& autoAgregar){
+	for(const auto& id : listaVehiculos){
+		if(id.modelo == autoAgregar.modelo){
+			cout<<"modelo ya agregado"<<autoAgregar.modelo<<" ya existe"<<endl;
+			return;
+		}
+	}
+	listaVehiculos.push_back(autoAgregar);
+	cout<<"Modelo agregadodo con existo"<<autoAgregar.modelo<<endl;
 }
 
