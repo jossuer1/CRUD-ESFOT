987e5bf51dc652685c0cf6a378c1dad9007dadf1
jossuer1 <josue.patino@epn.edu.ec>
2025-07-03 20:14:27 -0500

    Mejora Agregar: -Se genera ID Automaticamente. -Se creo un stock("cantidad") de vehiculos.

--- agregar.cpp
+++ agregar.cpp
@@ -12,21 +12,51 @@ struct vehiculos{
     string modelo;
     int año;
     double precio; 
+	int cantidad;
+
 };
+
 vector<vehiculos>listaVehiculos;
 
 vector<vehiculos> leer(){
 	return listaVehiculos;
 };
 
-void agregar(const auto& autoAgregar){
-	for(const auto& modelo : listaVehiculos){
-		if(modelo.id == autoAgregar.id){
-			cout<<"modelo ya agregado"<<autoAgregar.id<<" ya existe"<<endl;
+int contadorID = 1;
+
+void agregar(){
+	vehiculos nuevo;
+	nuevo.id = contadorID++;
+
+	cout<<"Marca: ";
+	getline(cin, nuevo.marca);
+	cout<<"Modelo: ";
+	getline(cin, nuevo.modelo);
+
+	while (true){
+		cout<<"Año: ";
+		cin>>nuevo.año;
+		if(nuevo.año >= 1950 && nuevo.año <=2025){
+			break;
+		}else{
+			cout<<"Año ingresado invalido (SOLO AUTOS ENTRE 1980 Y 2025)"<<endl;
+		}
+	}
+
+	cout<<"Precio: ";
+	cin>>nuevo.precio;
+	cin.ignore();
+
+	for(auto& v : listaVehiculos){
+		if(v.marca == nuevo.marca && v.modelo ==nuevo.modelo ){
+			v.cantidad += 1;
+			cout<<"Auto ya existente. Se añade a catalogo cone ID :"<<nuevo.id<<endl;
 			return;
 		}
 	}
-	listaVehiculos.push_back(autoAgregar);
-	cout<<"Modelo agregadodo con existo"<<autoAgregar.id<<endl;
+
+	nuevo.cantidad = 1;
+	listaVehiculos.push_back(nuevo);
+	cout<<"Nuevo modelo agregado con exito :3\n El ID generado es: "<<nuevo.id<<endl;
 }
 
