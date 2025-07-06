273e0e4dad97cb7d4914ab86e523c63fe47410c5
jossuer1 <josue.patino@epn.edu.ec>
2025-07-05 23:53:36 -0500

    Interfaz inicio: botones sin codigo y un metodo de escritura

--- Crud_Autos_QT/mainwindow.cpp
+++ Crud_Autos_QT/mainwindow.cpp
@@ -12,3 +12,28 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+}
+
