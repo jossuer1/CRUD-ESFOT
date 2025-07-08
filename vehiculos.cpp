792308acbc0a2b07d7f02f59ac4af65750aa0756
jossuer1 <josue.patino@epn.edu.ec>
2025-07-08 16:03:44 -0500

    cambios: se agrega vehiculos.cpp y se agrega vector a vehiculos.h

---
+++ vehiculos.cpp
@@ -0,0 +1,5 @@
+#include "vehiculos.h"
+
+// Definición de variables globales
+vector<Vehiculo> listaVehiculos;
+int contadorID = 1;
--- vehiculos.h
+++ vehiculos.h
@@ -2,6 +2,7 @@
 #define VEHICULOS_H
 
 #include <string>
+#include <vector>
 using namespace std;
 
 struct Vehiculo {
