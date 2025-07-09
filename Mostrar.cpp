57a8b5f50d34f3aa2da29bdb7df384846c09ff6b
Joelo05 <joelportillaldu@gmail.com>
2025-07-09 15:25:49 -0500

    Archivo mostrar.cpp y estructura inicial

--- Mostrar.cpp
+++ Mostrar.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 void mostrar() {
-    ifstream archivo("datos.txt");
-} 
+    ifstream archivo("datos.txt"); // Abrimos el archivo para leer
+    if (!archivo.is_open()) {
+        cout << "No se pudo abrir el archivo." << endl;
+        return;
+    }
+    // Luego leeremos el contenido
+    archivo.close();
+}
